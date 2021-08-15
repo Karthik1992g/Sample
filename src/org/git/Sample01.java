0fd656d7e457cca0c5f44f2003e69ea7a4f37aea
Karthik1992g <felliss92@gmail.com>
2021-08-15 17:51:16 +0530

    B

--- src/org/git/Sample01.java
+++ src/org/git/Sample01.java
@@ -5,8 +5,8 @@ public class Sample01 {
 	public static void main(String[] args) {
 		System.out.println("Python");
 		System.out.println("Selenium");
-		System.out.println("Junit");
-		System.out.println("Java");
+		System.out.println("abcd");
+		System.out.println("12345");
 		
 	}
 
