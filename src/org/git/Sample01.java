c673167116be32bad7b32972de920531dc9d35e9
Karthik1992g <felliss92@gmail.com>
2021-08-15 17:27:53 +0530

    A is done

--- src/org/git/Sample01.java
+++ src/org/git/Sample01.java
@@ -4,6 +4,7 @@ public class Sample01 {
 	
 	public static void main(String[] args) {
 		System.out.println("Python");
+		System.out.println("TestNG");
 		System.out.println("Selenium");
 		System.out.println("Java");
 	}
