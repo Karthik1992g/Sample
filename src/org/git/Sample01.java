86f39f3ce08e1831ffa82d954b38ceeae735eef1
Karthik1992g <felliss92@gmail.com>
2021-08-15 17:00:59 +0530

    A branch is updated

--- src/org/git/Sample01.java
+++ src/org/git/Sample01.java
@@ -3,8 +3,9 @@ package org.git;
 public class Sample01 {
 	
 	public static void main(String[] args) {
-		System.out.println("vimala");
-		System.out.println("Karthik");
+		System.out.println("Python");
+		System.out.println("Selenium");
+		System.out.println("Java");
 	}
 
 }
