75c09252dca0e2ea8bf3914c9fcf94ec41d9c7c4
Karthik1992g <felliss92@gmail.com>
2021-08-15 17:48:40 +0530

    A

--- src/org/git/Sample01.java
+++ src/org/git/Sample01.java
@@ -5,9 +5,7 @@ public class Sample01 {
 	public static void main(String[] args) {
 		System.out.println("Python");
 		System.out.println("TestNG");
-		System.out.println("Selenium");
-		System.out.println("Java");
-		System.out.println("cucumber");
+	
 	}
 
 }
